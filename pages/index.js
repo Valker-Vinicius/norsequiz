ed6457f654c4de93b1cdbf82500b997b5df767c8
Devinicius <valkerm2v@gmail.com>
2021-01-26 21:48:37 -0300

    class #2 finished

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,13 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Head from 'next/head'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-
-/*
-const BackGroundImage = styled.div`
-	background-image: url(${db.bg});
-	flex: 1;
-	background-size: cover;
-	background-position: center;
-`
-*/
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -24,41 +18,66 @@ export const QuizContainer = styled.div`
     margin: auto;
     padding: 15px;
   }
-`
+`;
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<meta property="og:title" content="Norse Quiz"/>
-				<meta property="og:image" content={db.metaImg}/>
-				<meta property="og:description" content="Come make sure that you are ready for the Valhalla!"/>
-			</Head>
-			<QuizBackground backgroundImage={db.bg}>
-				<QuizContainer>
-					<Widget>
-						<Widget.Header>
-							<h1>Norse Mythology Quiz</h1>
-						</Widget.Header>
-						<Widget.Content>
-							<p>
-								Make sure that you are ready for the Valhalla!
-							</p>
-						</Widget.Content>
-					</Widget>
-					<Widget>
-						<Widget.Content>
+  const router = useRouter();
+  const [name, setName] = React.useState('');
 
-							<h1>Other awesome quizzes!</h1>
-							<p>
-								loren ipsum dolor
-						</p>
-						</Widget.Content>
-					</Widget>
-					<Footer />
-				</QuizContainer>
-				<GitHubCorner projectUrl="https://github.com/valker-vinicius/norsequiz" />
-			</QuizBackground>
-		</>
-	)
+  return (
+    <>
+      <Head>
+        <title>Norse Quiz</title>
+        <link rel="shortcut icon" href="https://image.flaticon.com/icons/png/512/1377/1377698.png" />
+        <meta property="og:title" content="Norse Quiz" />
+        <meta property="og:image" content={db.metaImg} />
+        <meta property="og:description" content="Come make sure that you are ready for the Valhalla!" />
+      </Head>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>Norse Mythology Quiz</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>
+                Make sure that you are ready for the Valhalla!
+              </p>
+              <form onSubmit={function (event) {
+                event.preventDefault();
+                router.push(`/quiz?name=${name}`);
+              }}
+              >
+                <input
+                  onChange={function (event) {
+                    setName(event.target.value);
+                  }}
+                  placeholder="Tell me your name"
+                  required
+                />
+                <button type="submit" disabled={name.length === 0}>
+                  Are you ready
+                  {' '}
+                  {name}
+                  ?
+                </button>
+              </form>
+            </Widget.Content>
+          </Widget>
+          <Widget>
+            <Widget.Content>
+
+              <h1>Other awesome quizzes!</h1>
+              <p>
+                loren ipsum dolor
+              </p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/valker-vinicius/norsequiz" />
+      </QuizBackground>
+    </>
+  );
 }
